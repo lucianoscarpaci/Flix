1c1125ca7a9653e32b57475cd05940eac3c368a9
Luciano Scarpaci <Scarpacii.luciano@gmail.com>
2022-09-09 21:58:53 -0400

    Added Superhero Collection View

---
+++ NewsFlix/NewsFlix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  NewsFlix
+//
+//  Created by luciano scarpaci on 9/8/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
